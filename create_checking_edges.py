8f0e68a58814723becd096040071152abb4ee75b
Ubuntu <ubuntu@ip-172-31-33-103.us-west-2.compute.internal>
2016-12-08 18:56:05 +0000

    modified checking edge creation

--- create_checking_edges.py
+++ create_checking_edges.py
@@ -1,18 +1,21 @@
-import zeus
+import zeusdb
 import json
-import time
+from time import time
+import sys
 
-INPUT_FILE = 'checkin_edges.txt'
+INPUT_FILE = 'graph_data/checkin_edges.txt'
 total = sum(1 for line in open(INPUT_FILE))
 print 'Total edges =',total
 start_time = time()
 with open(INPUT_FILE) as infile:
-    for line in infile:
-    	print type(line)
-    	edge = eval(unicode(line))
-		zeusdb.create_edge(edge)
-		sys.stdout.write("\rCreated "+str(i)+" edges out of "+str(total)+" edges")
-		sys.stdout.flush()
+    for i,line in enumerate(infile):
+    	#print type(line)
+    	if i<470:
+		continue
+	edge = eval(unicode(line))
+	zeusdb.create_edge(edge)
+	sys.stdout.write("\rCreated "+str(i)+" edges out of "+str(total)+" edges")
+	sys.stdout.flush()
 end_time = time()
 print
 print "It took " + str(end_time-start_time) + " seconds to create " + str(total) + " edges"
